9e6374280955db1f01acb356963669b8d4d91016
Tom Roar Furunes <tom.roar@halloverden.no>
2020-09-16 19:14:21 +0200

    Initial commit

---
+++ DependencyInjection/HalloVerdenDoctrineSqlLoggerExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace HalloVerden\DoctrineSqlLoggerBundle\DependencyInjection;
+
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+/**
+ * Class HalloVerdenDoctrineSqlLoggerExtension
+ *
+ * @package HalloVerden\DoctrineSqlLoggerBundle\DependencyInjection
+ */
+class HalloVerdenDoctrineSqlLoggerExtension extends Extension {
+
+  /**
+   * @inheritDoc
+   */
+  public function load(array $configs, ContainerBuilder $container) {
+
+  }
+
+}
---
+++ HalloVerdenDoctrineSqlLoggerBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace HalloVerden\DoctrineSqlLoggerBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class HalloVerdenDoctrineSqlLoggerBundle
+ *
+ * @package HalloVerden\DoctrineSqlLoggerBundle
+ */
+class HalloVerdenDoctrineSqlLoggerBundle extends Bundle {
+
+}
