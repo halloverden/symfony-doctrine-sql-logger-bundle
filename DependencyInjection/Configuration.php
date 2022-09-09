9f0e6af413ed82fbe0ae4f38b7bf46f3bdfb0770
Tom Roar Furunes <tom.roar@halloverden.no>
2022-09-09 15:40:19 +0200

    ADD: return type

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -14,7 +14,7 @@ class Configuration implements ConfigurationInterface {
   /**
    * @inheritDoc
    */
-  public function getConfigTreeBuilder() {
+  public function getConfigTreeBuilder(): TreeBuilder {
     $treeBuilder = new TreeBuilder(self::NAME);
 
     $treeBuilder->getRootNode()
